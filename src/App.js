4548d338dee50b2d151567b75cd7bdd614cb8835
tonyhdz404 <antonio.hernandez030@gmail.com>
2022-06-23 11:16:13 -0600

    Finished All Tests Passed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { VscRefresh, VscTwitter } from "react-icons/vsc";
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [qoute, setQoute] = useState({});
+  async function fetchData() {
+    setIsLoading(true);
+    const res = await fetch("https://api.quotable.io/random");
+    const data = await res.json();
+    setQoute(data);
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <VscRefresh className="loading__icon" />
+      </div>
+    );
+  }
   return (
     <div id="quote-box">
-      <h1>Hello</h1>
+      <p id="text">{qoute.content}</p>
+      <span id="author">- {qoute.author}</span>
+      <div className="button-wrapper">
+        <button id="new-quote" type="button" onClick={fetchData}>
+          New qoute
+        </button>
+        <a href="twitter.com/intent/tweet" id="tweet-quote" target="_blank">
+          <VscTwitter className="icon" />
+        </a>
+      </div>
     </div>
   );
 }
